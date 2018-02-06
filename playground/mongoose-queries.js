7ec47b9f9d7b44199533c3d7b45649c3c898feab
apverdin <adam.verdin@gmail.com>
2018-02-05 20:04:36 -0500

    add queries playground

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,41 @@
+
+const {mongoose} = require('./../server/db/mongoose');
+const {ObjectID} = require('mongodb')
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+// var id = '5a75d29861bae7e4136664ef';
+
+// if (!ObjectID.isValid(id)) {
+//     console.log("ID is not valid");
+// };
+
+
+// Todo.find({
+//     _id: id
+// }).then((todos) => {
+//     console.log('Todos', todos);
+// });
+
+// Todo.findOne({
+//     _id: id
+// }).then((todo) => {
+//     console.log('Todo', todo);
+// });
+
+// Todo.findById(id).then((todo) => { 
+//     if(!todo){
+//     return console.log("ID not found");
+//     }
+//     else {
+//     console.log('Todo by Id', todo)
+//     }
+// }).catch((e) => console.log(e));;
+var userID = '5a72f6dac37ebb7ae5c9315a';
+User.findById(userID).then((user) => { 
+    if(!user){
+    return console.log("ID not found for User");
+    }
+    else {
+    console.log(JSON.stringify(user, undefined,2));
+    }
+}).catch((e) => console.log(e));;
