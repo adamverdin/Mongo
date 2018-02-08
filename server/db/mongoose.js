8b939e60b38459a3b00d3a4bf7f6e8d2ff0146d3
apverdin <adam.verdin@gmail.com>
2018-02-07 22:19:40 -0500

    resolve database

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,11 +2,6 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-let db = {
-    localhost: 'mongodb://localhost:27017/TodoApp',
-    mlab: 'mongodb://<dbuser>:<dbpassword>@ds229468.mlab.com:29468/todo-app'
-  };
-
-mongoose.connect(db.localhost || db.mlab);
+mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds229468.mlab.com:29468/todo-app');
 
 module.exports = {mongoose};
