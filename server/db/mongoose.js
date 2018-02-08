fc207f489551ffe2c639b65e4bbdb67a3cf7fcb6
apverdin <adam.verdin@gmail.com>
2018-02-08 18:06:46 -0500

    mlab added in

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect( process.env.PORT ? 'mongodb://admin:Artman09!@ds229468.mlab.com:29468/todo-app' : 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://admin:Artman09!@ds229468.mlab.com:29468/todo-app');
 
 // mongoose.connect('mongodb://admin:Artman09!@ds229468.mlab.com:29468/todo-app');
 
