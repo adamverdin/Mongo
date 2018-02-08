f0a9180e34dfb40926b8cd67268ca1ed9a29ad95
apverdin <adam.verdin@gmail.com>
2018-02-07 19:48:47 -0500

    api developed

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var bodyParser = require('body-parser')
+const {ObjectID} = require('mongodb')
 
-var {mongoose} = require('./db/mongoose.js');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+var {
+    mongoose
+} = require('./db/mongoose.js');
+var {
+    Todo
+} = require('./models/todo');
+var {
+    User
+} = require('./models/user');
 
 var app = express();
 
@@ -12,8 +19,8 @@ app.use(bodyParser.json());
 app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
-    })
-    ;
+    });
+
     todo.save().then((doc) => {
         res.send(doc)
     }, (e) => {
@@ -23,15 +30,51 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
-        res.send({todos});
+        res.send({
+            todos
+        });
     }, (e) => {
         res.status(400).send(e);
-    })
-})
+    });
+});
 
+
+//fetch query string in api url
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    //es.send(req.params);
+    //validate id using isValid
+    // if (!ObjectID.isValid(id)) {
+    // console.log("ID is not valid");
+    // };
+    if (!ObjectID.isValid(id)) {
+    return res.status(400).send();
+    };
+
+    //if Invalid respond with 404-send back empty body
+    //find by id
+Todo.findById(id).then((todo) => { 
+    if(!todo){
+        return res.status(400).send();
+    }
+    else {
+        res.send({todo});
+    }
+}).catch((e) => {
+    res.statusCode(400).send();
+});;
+    //success
+    //if todo there -> send back
+    //if no to do -> send back 404 w/ empty body.
+    //error
+    //400 -> and send empty boy back
+});
 
 app.listen(3000, () => {
     console.log('started on port 3000');
 })
 
-module.exports = {app}
+module.exports = {
+    app
+}
