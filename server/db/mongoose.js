5607208242708f749773ba3a16a1b9fc7e248658
apverdin <adam.verdin@gmail.com>
2018-02-08 07:27:00 -0500

    pushed to heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,10 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://admin:Artman09!@ds229468.mlab.com:29468/todo-app');
+mongoose.connect( process.env.PORT ? 'mongodb://apverdin:Artman09!@ds229468.mlab.com:29468/todo-app' : 'mongodb://localhost:27017/TodoApp');
+
+// mongoose.connect('mongodb://apverdin:Artman09!@ds229468.mlab.com:29468/todo-app');
 
 module.exports = {mongoose};
+
+//mongoose.connect('mongodb://localhost:27017/TodoApp');
