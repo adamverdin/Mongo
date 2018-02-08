76cd69dafca3780bed00e19192de886b0821f8d0
apverdin <adam.verdin@gmail.com>
2018-02-08 18:03:18 -0500

    FIXED PORT

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,10 +2,11 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect( process.env.PORT ? 'mongodb://apverdin:Artman09!@ds229468.mlab.com:29468/todo-app' : 'mongodb://localhost:27017/TodoApp');
+mongoose.connect( process.env.PORT ? 'mongodb://admin:Artman09!@ds229468.mlab.com:29468/todo-app' : 'mongodb://localhost:27017/TodoApp');
 
-// mongoose.connect('mongodb://apverdin:Artman09!@ds229468.mlab.com:29468/todo-app');
+// mongoose.connect('mongodb://admin:Artman09!@ds229468.mlab.com:29468/todo-app');
+
+// mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
 
-//mongoose.connect('mongodb://localhost:27017/TodoApp');
