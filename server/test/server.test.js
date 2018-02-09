b47455eb4364f28692c4b7d1cdd226d82b954095
apverdin <adam.verdin@gmail.com>
2018-02-08 22:05:31 -0500

    server test delete

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -23,7 +23,7 @@ const todos = [{
 beforeEach((done) => {
     Todo.remove({}).then(() => {
         return Todo.insertMany(todos);
-    }).then(() => done());
+    }).then(() => done())
 });
 
 describe('POST /todos', () => {
@@ -101,16 +101,54 @@ describe('Get /todos/:id', () => {
     it('should return a 404 error if todo not found', (done) => {
         var falseId = new ObjectID;
         request(app)
-        .get(`/todos/${falseId.toHexString()}`)
-        .expect(404)
-        .end(done)
+            .get(`/todos/${falseId.toHexString()}`)
+            .expect(404)
+            .end(done)
     });
 
     it('should return 404 for non-object IDs', (done) => {
-        request(app)// todoes/123
+        request(app) // todoes/123
             .get('/todos/123')
             .expect(404)
             .end(done)
     });
 });
 
+describe('DELETE /todos/:id', () => {
+    it('should remove a todo', (done) => {
+        var hexId = todos[1]._id.toHexString();
+
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo._id).toBe(hexId);
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                Todo.findById(hexId).then((todo) => {
+                    expect(todo).toNotExist();
+                    done();
+                }).catch((e) => done(e));
+            })
+
+    });
+
+    it('should return 404 if todo not found', (done) => {
+        var falseId = new ObjectID;
+        request(app)
+            .delete(`/todos/${falseId.toHexString()}`)
+            .expect(404)
+            .end(done)
+    });
+
+    it('should return 404 if object id is not found', (done) => {
+        request(app) // todoes/123
+            .delete('/todos/123')
+            .expect(404)
+            .end(done)
+    });
+});
